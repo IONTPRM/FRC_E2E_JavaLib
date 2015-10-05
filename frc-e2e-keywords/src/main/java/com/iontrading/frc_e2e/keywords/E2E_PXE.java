222693cc5152b2f38daf7fc78c7c9583640fd3f8
tilak-raj <t.raj@iontrading.com>
2015-10-05 20:00:43 +0530

    minor updates

--- frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_PXE.java
+++ frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_PXE.java
@@ -1,5 +1,90 @@
 package com.iontrading.frc_e2e.keywords;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywords;
+
+import com.iontrading.jmix.subscribe.function.IFunctionCallResult;
+import com.iontrading.robotframework.keywords2.FunctionRepository;
+
+@RobotKeywords
 public class E2E_PXE {
 
+	public static String PXE_SOURCE;
+	public static String PXE_CURRENCY;
+
+	private final FunctionRepository funcRep = new FunctionRepository();
+
+	public Map<String, Integer> anlPYCalcFuncResFieldValuePos = new HashMap<String, Integer>();
+	
+	public void setANLPYCalcFuncResFieldValuePos() {
+		anlPYCalcFuncResFieldValuePos.put("Status", 0);
+		anlPYCalcFuncResFieldValuePos.put("Price", 1);
+		anlPYCalcFuncResFieldValuePos.put("Yield", 2);
+		anlPYCalcFuncResFieldValuePos.put("Discount", 3);
+		anlPYCalcFuncResFieldValuePos.put("MMYield", 4);
+		anlPYCalcFuncResFieldValuePos.put("DateSettl", 5);
+		anlPYCalcFuncResFieldValuePos.put("AccruedInterest", 6);
+		anlPYCalcFuncResFieldValuePos.put("AccruedDays", 7);
+		anlPYCalcFuncResFieldValuePos.put("DV01", 8);
+		anlPYCalcFuncResFieldValuePos.put("IndexRatio", 9);
+		anlPYCalcFuncResFieldValuePos.put("RiskPrice", 10);
+		anlPYCalcFuncResFieldValuePos.put("DiscountMargin", 11);
+		anlPYCalcFuncResFieldValuePos.put("Factor", 12);
+	}
+	
+	/**
+	 * 
+	 * @param source
+	 * @param currency
+	 */
+	@RobotKeyword	
+	public void setPxeSourceAndCurrency(String source, String currency) {
+		
+		E2E_PXE.PXE_SOURCE = source;
+		E2E_PXE.PXE_CURRENCY = currency;	
+	}
+	
+	/**
+	 * 
+	 * @param fieldNameToGetValue
+	 * @param instrumentId
+	 * @param value
+	 * @param valueType
+	 * @param dateSettl
+	 * @return
+	 * @throws Exception
+	 */
+	public String getValueUsingAnlpycalcFunc(String fieldNameToGetValue, String instrumentId, String value, String valueType, String dateSettl) throws Exception {
+		
+		setANLPYCalcFuncResFieldValuePos();
+		
+		String fieldValueFromPXE = null;
+		
+		funcRep.functionDefine(PXE_SOURCE, "ANLPYCalc", "InstrumentId", instrumentId, "Value", value, "ValueType", valueType, "DateSettl", dateSettl);
+		funcRep.functionSetTimeout("20s");
+		funcRep.functionVerifyReturn("0", "OK");
+		IFunctionCallResult result = funcRep.functionCall();
+		
+		if (fieldNameToGetValue.equals("Price") || 
+				fieldNameToGetValue.equals("Yield") || 
+				fieldNameToGetValue.equals("Discount") || 
+				fieldNameToGetValue.equals("MMYield") || 
+				fieldNameToGetValue.equals("AccruedInterest") || 
+				fieldNameToGetValue.equals("DV01") ||
+				fieldNameToGetValue.equals("IndexRatio") ||
+				fieldNameToGetValue.equals("RiskPrice") ||
+				fieldNameToGetValue.equals("DiscountMargin") ||
+				fieldNameToGetValue.equals("Factor")) {
+			fieldValueFromPXE = result.getDouble(anlPYCalcFuncResFieldValuePos.get(fieldNameToGetValue)).toString();
+		} else if (fieldNameToGetValue.equals("DateSettl") || fieldNameToGetValue.equals("AccruedDays")) {
+			fieldValueFromPXE = result.getInteger(anlPYCalcFuncResFieldValuePos.get(fieldNameToGetValue)).toString();
+		} else {
+			return "";
+		}
+		
+		return fieldValueFromPXE;
+	}
 }
--- frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_Tradeserver.java
+++ frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_Tradeserver.java
@@ -1,5 +1,70 @@
 package com.iontrading.frc_e2e.keywords;
 
+import org.robotframework.javalib.annotation.*;
+
+import com.iontrading.frc_e2e.utils.*;
+
+import com.iontrading.robotframework.base.IReadableRecord;
+import com.iontrading.robotframework.keywords2.MkvRecordRepository;
+
+@RobotKeywords
 public class E2E_Tradeserver {
 
+    public static final String ROBOT_LIBRARY_DOC_FORMAT = "HTML";
+
+	public static String TS_SOURCE;
+	public static String TS_CURRENCY;
+	
+	private final MkvRecordRepository recordRepository = new MkvRecordRepository();  
+		
+	@RobotKeyword
+	public void setTradeserverSourceAndCurrency(String source, String currency) {
+		
+		E2E_Tradeserver.TS_SOURCE = source;
+		E2E_Tradeserver.TS_CURRENCY = currency;	
+	}
+	
+	public String getTSTradeRecordId(String tsTradeId) {
+		
+		E2E_Utility utility = new E2E_Utility();
+		String washedTradeId = utility.washName(tsTradeId);
+		return TS_CURRENCY + "_CM_TRADE_" + TS_SOURCE + "_" + washedTradeId;
+	}
+	
+	/**
+	 * Keyword to verify fields' values on trade record in Tradeserver
+	 * @param tradeId
+	 * @param fieldValuePairs
+	 * @throws Exception
+	 */
+	@RobotKeyword
+	public void verifyTradeserverFields(String tradeId, Object[] fieldValuePairs) throws Exception {
+
+		//String cmTradeRecordName = getTSTradeRecordId(tradeId);
+		E2E_Utility utility = new E2E_Utility();
+		E2E_PXE pxe = new E2E_PXE();
+		String washedTradeId = utility.washName(tradeId);
+		
+		String recName1 = recordRepository.recordDefine(TS_SOURCE, "CM_TRADE", TS_CURRENCY, washedTradeId);
+		recordRepository.recordSetTimeout(recName1, "5s");
+		IReadableRecord readableRecord = recordRepository.recordSubscribe(recName1);
+		recordRepository.recordClose(recName1);
+		
+		String instrumentId = readableRecord.getFieldValue("InstrumentId").toString();
+		String value = readableRecord.getFieldValue("Value").toString();
+		String valueType = readableRecord.getFieldValue("ValueType").toString();
+		String dateSettl = readableRecord.getFieldValue("DateSettl").toString();
+		
+		for (int i=0; i < fieldValuePairs.length; i=i+2) {
+			if (fieldValuePairs[i+1] == "value_ret_by_pxe") {
+				fieldValuePairs[i+1] = pxe.getValueUsingAnlpycalcFunc(fieldValuePairs[i].toString(), instrumentId, value, valueType, dateSettl);
+			}
+		}
+		
+		String recName2 = recordRepository.recordDefine(TS_SOURCE, "CM_TRADE", TS_CURRENCY, washedTradeId);
+		recordRepository.recordSetTimeout(recName2, "5s");
+		recordRepository.recordVerifyFields(recName2, fieldValuePairs);
+		recordRepository.recordClose(recName2);
+	
+	}
 }
