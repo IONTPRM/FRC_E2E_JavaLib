1b215d88497b6cb4721375133063b00071f7b808
Ankit Jain <ankit.jain@iontrading.com>
2015-12-08 17:19:28 +0530

    added annotation @@RobotKeywords for file E2E_RunlistManager.java

--- frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_RunlistManager.java
+++ frc-e2e-keywords/src/main/java/com/iontrading/frc_e2e/keywords/E2E_RunlistManager.java
@@ -1,12 +1,14 @@
 package com.iontrading.frc_e2e.keywords;
 
 import org.robotframework.javalib.annotation.RobotKeyword;
+import org.robotframework.javalib.annotation.RobotKeywords;
 
 import com.iontrading.frc_e2e.reusableutils.FunctionUtils;
 import com.iontrading.frc_e2e.utils.RobotLogger;
 import com.iontrading.frc_e2e.utils.SetServerSourceCurrency;
 import com.iontrading.jmix.subscribe.function.IFunctionCallResult;
 
+@RobotKeywords
 public class E2E_RunlistManager {
 	public static final String ROBOT_LIBRARY_SCOPE = "TEST CASE";
 	private static final RobotLogger htmlLogger = RobotLogger.getLogger(E2E_Stpserver.class.getName());
